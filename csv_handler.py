93d43fea675bc99288cdfdd1e19a6271d76b722d
CianMcDonald <cianmcdonald2001@gmail.com>
2022-10-16 19:33:32 +0100

    added ability to search for a keyword

---
+++ csv_handler.py
@@ -0,0 +1,66 @@
+# For saving the response data in CSV format
+import csv
+# For parsing the dates received from twitter in readable formats
+import datetime
+import dateutil.parser
+import unicodedata
+
+def create_csv():
+    # Create file
+    csvFile = open("cost_of_living_hashtags.csv", "a", newline="", encoding='utf-8')
+    csvWriter = csv.writer(csvFile)
+
+    #Create headers for the data you want to save, in this example, we only want save these columns in our dataset
+    csvWriter.writerow(['hashtag', 'tweet_id', 'tweet', 'created_at', 'geo', 'author_id' ,'lang', 'like_count', 'quote_count', 'reply_count','retweet_count'])
+
+def append_to_csv(json_response, fileName):
+
+    #A counter variable
+    counter = 0
+
+    #Open OR create the target CSV file
+    csvFile = open(fileName, "a", newline="", encoding='utf-8')
+    csvWriter = csv.writer(csvFile)
+
+    #Loop through each tweet
+    for tweet in json_response['data']:
+        for hashtag in tweet['entities']['hashtags']:
+            # 1. Author ID
+            author_id = tweet['author_id']
+
+            # 2. Time created
+            created_at = dateutil.parser.parse(tweet['created_at'])
+
+            # 3. Geolocation
+            if ('geo' in tweet):   
+                geo = tweet['geo']['place_id']
+            else:
+                geo = " "
+
+            # 4. Tweet ID
+            tweet_id = tweet['id']
+
+            # 5. Language
+            lang = tweet['lang']
+
+            # 6. Tweet metrics
+            retweet_count = tweet['public_metrics']['retweet_count']
+            reply_count = tweet['public_metrics']['reply_count']
+            like_count = tweet['public_metrics']['like_count']
+            quote_count = tweet['public_metrics']['quote_count']
+
+            # 8. Tweet text
+            text = tweet['text']
+            
+            # 'hashtag', 'tweet_id', 'tweet', 'created_at', 'geo', 'author_id' ,'lang', 'like_count', 'quote_count', 'reply_count','retweet_count'
+            res = [hashtag['tag'], tweet_id, text, created_at, geo, author_id, lang, like_count, quote_count, reply_count, retweet_count]
+            
+            # Append the result to the CSV file
+            csvWriter.writerow(res)
+            counter += 1
+
+        # When done, close the CSV file
+    csvFile.close()
+
+    # Print the number of tweets for this iteration
+    print("# of Tweets added from this response: ", counter)
---
+++ main.py
@@ -0,0 +1,56 @@
+# For sending GET requests from the API
+import requests
+# For saving access tokens and for file management when creating and adding to the dataset
+import os
+# For dealing with json responses we receive from the API
+import json
+# For displaying the data after
+import pandas as pd
+
+#To add wait time between requests
+import time
+
+from csv_handler import append_to_csv
+
+def auth():
+    return os.getenv('TOKEN')
+
+def create_headers(bearer_token):
+    headers = {"Authorization": "Bearer {}".format(bearer_token)}
+    return headers
+
+def create_url(keyword, max_results = 5):
+    
+    search_url = "https://api.twitter.com/2/tweets/search/recent" #Change to the endpoint you want to collect data from
+
+    #change params based on the endpoint you are using
+    query_params = {'query': keyword,
+                    # 'start_time': start_date,
+                    # 'end_time': end_date,
+                    'max_results': max_results,
+                    'expansions': 'author_id,in_reply_to_user_id,geo.place_id',
+                    'tweet.fields': 'id,text,author_id,in_reply_to_user_id,geo,conversation_id,created_at,lang,public_metrics,referenced_tweets,reply_settings,source,entities',
+                    'user.fields': 'id,name,username,created_at,description,public_metrics,verified',
+                    'place.fields': 'full_name,id,country,country_code,geo,name,place_type',
+                    'next_token': {}}
+    return (search_url, query_params)
+
+def connect_to_endpoint(url, headers, params, next_token = None):
+    params['next_token'] = next_token   #params object received from create_url function
+    response = requests.request("GET", url, headers = headers, params = params)
+    print("Endpoint Response Code: " + str(response.status_code))
+    if response.status_code != 200:
+        raise Exception(response.status_code, response.text)
+    return response.json()
+
+if __name__ == '__main__':
+    bearer_token = auth()
+    headers = create_headers(bearer_token)
+    keyword = "Cost of living -is:retweet -is:reply has:hashtags"
+    max_results = 15
+
+    while True:
+        url = create_url(keyword, max_results)
+        json_response = connect_to_endpoint(url[0], headers, url[1])
+        append_to_csv(json_response, "cost_of_living_hashtags.csv")
+        time.sleep(600)
